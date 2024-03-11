92b06dbfb5b3256eccffa5924add06503b7146ba
ManuelMaccou <93491644+ManuelMaccou@users.noreply.github.com>
2024-03-10 23:24:26 -0400

    Update sendTransaction.js

--- backend/api/marketplace/frame/sendTransaction.js
+++ backend/api/marketplace/frame/sendTransaction.js
@@ -58,8 +58,12 @@ router.post('/:productId', async (req, res) => {
 
     const response = {
       chainId: "eip155:8453", // Base
-      to: "0x4A6737Da9668D09aCE702c3ff5e0dA33a84d28F7",
-      value: weiEquivalent.toString(),
+      method: "eth_sendTransaction",
+      params: {
+        to: "0x4A6737Da9668D09aCE702c3ff5e0dA33a84d28F7",
+        value: weiEquivalent.toString(), // Use the converted value
+        data: "" // Optional for ETH transfers
+      },
     };
 
     res.status(200).json(response); // Send JSON response
