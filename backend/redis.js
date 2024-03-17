acb63555705f6f9668d9de4dba288952690c2175
ManuelMaccou <93491644+ManuelMaccou@users.noreply.github.com>
2024-03-17 15:51:22 -0400

    Update redis.js

--- backend/redis.js
+++ backend/redis.js
@@ -1,7 +1,7 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-    url: process.env.REDIS_PRIVATE_URL
+    url: process.env.REDIS_URL
 });
 
 client.on('error', (error) => console.error(`Redis Client Error: ${error}`));
@@ -14,5 +14,8 @@ const connectRedis = async () => {
         console.error('Failed to connect to Redis:', error);
     }
 };
+
+console.log('REDIS_PRIVATE_URL:', process.env.REDIS_PRIVATE_URL);
+
 
 export { client, connectRedis };
