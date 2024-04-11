15f2dc9177ab5b2f6dc7d4609190b8d03e174ea1
atlasmoth <ebuka422@gmail.com>
2024-04-11 20:47:26 +0100

    first commit in branch

--- gogh/src/utils/goghContract.ts
+++ gogh/src/utils/goghContract.ts
@@ -1,5 +1,7 @@
-import { ethers } from "ethers";
+import { JsonRpcSigner, ethers } from "ethers";
 import GOGH_ABI from "./goghAbi.json";
+import axios from "axios";
+import { Web3BaseProvider } from "web3";
 
 export const GOGH_CONTRACT_ADDRESS =
   "0x3c1A0A632B516cedFFEd053DFE18775D47d4B32c";
@@ -28,6 +30,119 @@ export const createEscrow = async (obj: {
 
   return createTx;
 };
+export const releaseEscrow = async (obj: {
+  transaction: Transaction;
+  signer: JsonRpcSigner;
+  contract: ethers.Contract;
+  accessToken: string;
+  provider: Web3BaseProvider;
+}) => {
+  let escrow = await obj.contract.getEscrowDetails(
+    obj?.transaction?.metadata?.escrowId
+  );
+  let escrowData = toEscrow(escrow);
+  if (escrowData.canceled || escrowData.released) {
+    throw new Error("Invalid action");
+  }
+  const address = await obj.signer.getAddress();
+  const messageSignerAddress = address.toLowerCase().trim();
+  const owner = escrowData.owner.toLowerCase().trim();
+  const recipient = escrowData.recipient.toLowerCase().trim();
+  if (![owner, recipient].includes(messageSignerAddress)) {
+    throw new Error("Unauthorized");
+  }
+  const coder = new ethers.AbiCoder();
+  const payload = coder.encode(
+    ["address", "address", "uint256", "address", "address"],
+    [
+      escrowData.escrowId,
+      escrowData.token,
+      escrowData.amount,
+      escrowData.recipient,
+      escrowData.owner,
+    ]
+  );
+  const escrowMessageHash = ethers.keccak256(payload);
+  const messageHashBinary = ethers.getBytes(escrowMessageHash);
+
+  let transactionObj: Partial<Transaction> = {};
+
+  const signedMessage = await obj.signer.signMessage(messageHashBinary);
+
+  if (owner === messageSignerAddress) {
+    transactionObj.ownerSignature = signedMessage;
+  }
+  if (recipient === messageSignerAddress) {
+    transactionObj.recipientSignature = signedMessage;
+  }
+  // upload to DB
+  const { data: transaction } = (await axios.put(
+    `${process.env.REACT_APP_BASE_URL}/api/transaction/${obj.transaction.transactionHash}`,
+    transactionObj,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${obj.accessToken}`,
+      },
+    }
+  )) as { data: Transaction };
+  if (
+    !transaction.ownerSignature ||
+    !transaction.recipientSignature ||
+    owner !== messageSignerAddress
+  ) {
+    return { success: true, completeEscrow: false, transaction };
+  }
+  const releaseTx = await obj.contract.releaseEscrow(
+    obj.transaction.metadata.escrowId,
+    transaction.ownerSignature,
+    transaction.recipientSignature
+  );
+  return new Promise(async (resolve: Function, reject) => {
+    try {
+      obj.provider.once(releaseTx.hash, async () => {
+        try {
+          const { data: transaction } = await axios.put(
+            `${process.env.REACT_APP_BASE_URL}/api/transaction/${obj.transaction.transactionHash}`,
+            { metadata: { ...obj.transaction.metadata, released: true } },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${obj.accessToken}`,
+              },
+            }
+          );
+          resolve({
+            success: true,
+            completeEscrow: true,
+            transaction,
+            hash: releaseTx.hash,
+          });
+        } catch (error) {
+          reject(error);
+        }
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+interface Transaction {
+  metadata: Escrow ;
+  ownerSignature: string;
+  recipientSignature: string;
+  uid: string;
+  escrowId: string;
+  buyer: string;
+  seller: string;
+  buyerFid: string;
+  sellerFid: string;
+  sellerProfile: string;
+  sellerUsername: string;
+  transactionHash:string;
+  source:string;
+  marketplaceProduct: string;
+}
 
 // refactor later
 export const deleteEscrow = async (obj: {
