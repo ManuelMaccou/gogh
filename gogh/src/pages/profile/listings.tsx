731b2f9e9e710aa7ec9c2945bda1108bb14b9434
ManuelMaccou <93491644+ManuelMaccou@users.noreply.github.com>
2024-04-14 15:53:17 -0400

    Added prompt to connect wallet

--- gogh/src/pages/profile/listings.tsx
+++ gogh/src/pages/profile/listings.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
-import { usePrivy,useWallets } from '@privy-io/react-auth';
+import { usePrivy, useWallets } from '@privy-io/react-auth';
 import { useUser } from '../../contexts/userContext';
 import Header from '../header';
 import { cancelEscrow, getGoghContract, releaseEscrow } from '../../utils/goghContract';
@@ -56,88 +56,91 @@ interface Product {
     product: Product;
   }
 const PurchasesPage: React.FC = () => {
-  const [listings, setListings] = useState<MarketplaceProduct[]>([]);
-  const [marketplaceTransactions,setMarketplaceTransactions] = useState<ProductTransaction[]>([]);
-  const { user } = useUser();
+    const [listings, setListings] = useState<MarketplaceProduct[]>([]);
+    const [marketplaceTransactions,setMarketplaceTransactions] = useState<ProductTransaction[]>([]);
+    const { user } = useUser();
+    const { ready, authenticated, getAccessToken, logout } = usePrivy();
+    const {wallets} = useWallets();
+    const wallet = wallets[0];
 
-  const { ready, authenticated, getAccessToken, logout } = usePrivy();
-const {wallets} = useWallets();
-const wallet = wallets[0];
-const warmUpEtherDetails = useCallback(async() => {
-    const ethersProvider = await wallet.getEthersProvider();
-    const signer = ethersProvider.getSigner();
-    const goghContract = getGoghContract(signer);
-    const accessToken = await getAccessToken();
-    return {ethersProvider,signer,goghContract,accessToken }
-},[wallet,getAccessToken])
+    const {connectWallet} = usePrivy();
 
-  useEffect(() => {
-    const fetchListings = async () => {
-        const accessToken = await getAccessToken();
-
-        try {
-            const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/user/listings`, {
-                headers: { Authorization: `Bearer ${accessToken}` },
-            });
-            setListings(response.data);
-            setMarketplaceTransactions(response.data.flatMap((t:any) => t.transactions.map((a:any) => ({...a, product : {...t, transactions:null}}))))
-        } catch (error) {
-            console.error('Failed to fetch transactions:', error);
+    const warmUpEtherDetails = useCallback(async() => {
+        if (!wallet) {
+            connectWallet();
         }
-    };
 
-    if (user) {
-        fetchListings();
-    }
-  }, [user]);
+        const ethersProvider = await wallet.getEthersProvider();
+        const signer = ethersProvider.getSigner();
+        const goghContract = getGoghContract(signer);
+        const accessToken = await getAccessToken();
+        return {ethersProvider, signer, goghContract, accessToken }
+    },[wallet, getAccessToken])
 
-  // Trigger escrow signing here
+    useEffect(() => {
+        const fetchListings = async () => {
+            const accessToken = await getAccessToken();
 
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/user/listings`, {
+                    headers: { Authorization: `Bearer ${accessToken}` },
+                });
+                setListings(response.data);
+                setMarketplaceTransactions(response.data.flatMap((t:any) => t.transactions.map((a:any) => ({...a, product : {...t, transactions:null}}))))
+            } catch (error) {
+                console.error('Failed to fetch transactions:', error);
+            }
+        };
 
-  return (
-    <>
-    <Header />
-    <div className='profile-page'>
-        <div className="products-container">
-            <h2>Transactions</h2>
-            {marketplaceTransactions.map((listing) => (
-                <div key={listing._id} className="product-row">
-                    <img src={listing.product.featuredImage} alt="Product" className="product-image" />
-                    <div>
-                        <h3>{listing.product.title}</h3>
-                        {/* Placeholder for the transaction status */}
-                        <p>Status: Pending</p>
+        if (user) {
+            fetchListings();
+        }
+    }, [user]);
+
+    return (
+        <>
+        <Header />
+        <div className='profile-page'>
+            <div className="products-container">
+                <h2>Transactions</h2>
+                {marketplaceTransactions.map((listing) => (
+                    <div key={listing._id} className="product-row">
+                        <img src={listing.product.featuredImage} alt="Product" className="product-image" />
+                        <div>
+                            <h3>{listing.product.title}</h3>
+                            {/* Placeholder for the transaction status */}
+                            <p>Status: Pending</p>
+                        </div>
+                        <div>
+                            <button onClick={async()  => {
+                                try {
+                                    const {ethersProvider,signer,goghContract,accessToken} = await warmUpEtherDetails();
+                                    const res = await cancelEscrow({transaction : listing as any, signer : signer as any, contract : goghContract, accessToken : accessToken as string, provider : ethersProvider as any});
+                                    // successfully cancelled escrow
+                                    console.log(res)
+                                } catch (error) {
+                                    console.error('Unexpected error in cancel escrow:', error);
+                                    alert('An unexpected error occurred. Please try again.');
+                                }
+                            }} className="cancel-button">Cancel</button>
+                            <button onClick={async()  => {
+                                try {
+                                    const {ethersProvider,signer,goghContract,accessToken} = await warmUpEtherDetails();
+                                    const res = await releaseEscrow({transaction : listing as any, signer : signer as any, contract : goghContract, accessToken : accessToken as string, provider : ethersProvider as any});
+                                    // successfully signed escrow
+                                    console.log(res)
+                                } catch (error) {
+                                    console.error('Unexpected error in release escrow:', error);
+                                    alert('An unexpected error occurred. Please try again.');
+                                }
+                            }} className="confirm-button">Confirm</button>
+                        </div>
                     </div>
-                    <div>
-                        <button onClick={async()  => {
-                            try {
-                                const {ethersProvider,signer,goghContract,accessToken} = await warmUpEtherDetails();
-                                const res = await cancelEscrow({transaction : listing as any, signer : signer as any, contract : goghContract, accessToken : accessToken as string, provider : ethersProvider as any});
-                                // successfully cancelled escrow
-                                console.log(res)
-                            } catch (error) {
-                                console.error('Unexpected error in cancel escrow:', error);
-                                alert('An unexpected error occurred. Please try again.');
-                            }
-                        }} className="cancel-button">Cancel</button>
-                        <button onClick={async()  => {
-                            try {
-                                const {ethersProvider,signer,goghContract,accessToken} = await warmUpEtherDetails();
-                                const res = await releaseEscrow({transaction : listing as any, signer : signer as any, contract : goghContract, accessToken : accessToken as string, provider : ethersProvider as any});
-                                // successfully signed escrow
-                                console.log(res)
-                            } catch (error) {
-                                console.error('Unexpected error in cancel escrow:', error);
-                                alert('An unexpected error occurred. Please try again.');
-                            }
-                        }} className="confirm-button">Confirm</button>
-                    </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
-    </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default PurchasesPage;
